c9f50a39aabef5428c7df5e271f89ebae2910fc9
orionbrowne <orion.browne@gmail.com>
2021-06-17 20:35:06 -0400

    added starter code
    added starter code

---
+++ src/components/AccountBalance.js
@@ -0,0 +1,16 @@
+// src/components/AccountBalance.js
+
+import React, {Component} from 'react';
+
+
+class AccountBalance extends Component {
+  render() {
+    return (
+        <div>
+          Balance: {this.props.accountBalance}
+        </div>
+    );
+  }
+}
+
+export default AccountBalance;
