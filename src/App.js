6ac4e727a61d043635e350ef2637688346025be8
orionbrowne <orion.browne@gmail.com>
2021-06-29 16:27:51 -0400

    typo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
-import Login from './Login'
+import LogIn from './Login'
 
 class App extends Component {
 
